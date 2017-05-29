0d72624658a3f3d18e29e241d96a37c61b4c39fa
florian-neblich <florianneblich@gmail.com>
2017-05-29 21:26:51 +0200

    Einbau Hauptmenü. "Spiel beenden" Skript wurde hinzugefügt (funktioniert) sowie "Spiel starten"  Skript (funktioniert noch nicht so ganz).
    Einbau des Hauptmenüs in Unity. "Spiel beenden" Skript wurde hinzugefügt
    (funktioniert) sowie "Spiel starten"  Skript (funktioniert noch nicht so
    ganz).

---
+++ CMD_Run/Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour {
+
+    public void QuitGame()
+    {
+        Debug.Log("Butten geklickt, Spiel beenden");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
---
+++ CMD_Run/Assets/Scripts/StartOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartOnClick : MonoBehaviour {
+
+    public string sceneName;
+
+	public void StartLevel()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
